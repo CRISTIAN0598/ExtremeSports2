e77a20bf85e5ab087cba80a6a03a144e6238dd07
Cristian <kriztian31marulanda@gmail.com>
2023-08-21 21:49:27 -0500

    Create combos helper

---
+++ ExtremeSports2/Helpers/ICombosHelper.cs
@@ -0,0 +1,18 @@
+﻿using ExtremeSports2.Data.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ExtremeSports2.Helpers
+{
+    public interface ICombosHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetComboCategoriesAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboCategoriesAsync(IEnumerable<Category> filter);
+
+        Task<IEnumerable<SelectListItem>> GetComboCountriesAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboStatesAsync(int countryId);
+
+        Task<IEnumerable<SelectListItem>> GetComboCitiesAsync(int stateId);
+    }
+}
